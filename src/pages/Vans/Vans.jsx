eaf944bb5d2316a5b22d55493c9bea22d8654d6e
Kate Ives <katie.ives30@gmail.com>
2023-02-25 11:57:52 -0600

    add query parameter functionality

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
 
 export default function Vans(){
 
     const [vans, setVans] = useState([])
 
+    const [ searchParams, setSearchParams ]  = useSearchParams()
+
+    const typeFilter = searchParams.get('type')
+    
     useEffect(() => {
         fetch('/api/vans')
         .then(res => res.json())
         .then(data => setVans(data.vans))
     },[])
    
+    const filteredVans = typeFilter ? vans.filter(van => van.type.toLowerCase() === typeFilter) : vans
 
-    const vanElements = vans.map(van => {
+    const vanElements = filteredVans.map(van => {
         return(
             <div key={van.id}>
-            <img src={van.imageUrl}/>
-            <h2>{van.name}</h2>
-            <p>${van.price}/day</p>
-            <span>{van.type}</span>
+                <Link to={`/vans/${van.id}`}>
+                    <img src={van.imageUrl}/>
+                    <h2>{van.name}</h2>
+                    <p>${van.price}/day</p>
+                    <span>{van.type}</span>
+                </Link>
             </div>
         )
     })
@@ -26,6 +34,13 @@ export default function Vans(){
     return(
         <section>
             <h1>Explore our van options</h1>
+            <section>
+                <button onClick={() => setSearchParams({type: 'simple'})}>Simple</button>
+                <button onClick={() => setSearchParams({type: 'rugged'})}>Rugged</button>
+                <button onClick={() => setSearchParams({type: 'luxury'})}>Luxury</button>
+                <button onClick={() => setSearchParams({})}>Clear Filters</button>
+               
+            </section>
             <section>
                {vanElements}
             </section>
